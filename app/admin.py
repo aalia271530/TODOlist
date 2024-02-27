89579981e69b69073591197e24da5684f215716c
aalia271530 <139776021+aalia271530@users.noreply.github.com>
2024-02-27 17:10:25 +0530

    1st commit

---
+++ app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+admin.site.register(models.TODO)
+
+
+'''
+superuser
+username: aalia, aalia_
+pwd: 123, 123@aalia
+'''
